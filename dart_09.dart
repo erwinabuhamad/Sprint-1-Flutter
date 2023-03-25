bceb5bfcd8368125c68de7556b5787a5dcfc07f2
erwinabuhamad <128515930+erwinabuhamad@users.noreply.github.com>
2023-03-25 14:00:04 +0400

    Contoh penggunaan const, final dan variable scope

--- dart_09.dart
+++ dart_09.dart
@@ -1,1 +1,35 @@
+//Variable Scope , Const, Finally
+
+void main() {
+  var test;
+  
+  const test2 = "apa kabar const";  //value test2 tidak dapat berubah 
+  final test3 = "apa kabar final";  //value test3 tidak dapat berubah
+  
+  
+  /* INI AKAN ERROR, KARENA CONST WAJIB DIISI SAAT BUAT VARIABLE
+   const test2;
+   test2 = "apa kabar"; 
+   */
+  
+   
+  /* INI TIDAK ERROR, KARENA VALUE FINAL BISA  SETELAH VARIABLE DIBUAT
+   final test2;
+   test2 = "apa kabar"; 
+   */
+    
+  
+  
+  //Tujuannya adalah agar tidak terjadi bugs, contohnya penggunaan Phi. yang nilainya sudah pasti 3.14
+  
+  void myFunc()
+  {
+    test = 'hello';
+  }
+  
+  myFunc();
+  
+  print (test);
+}
+
 
