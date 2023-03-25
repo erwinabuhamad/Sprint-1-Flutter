697ef6e02dd50a69628bca351360de5f752e36c1
erwinabuhamad <128515930+erwinabuhamad@users.noreply.github.com>
2023-03-25 14:21:27 +0400

    Operator Null Safety  !,  ?  ,  late

--- dart_10.dart
+++ dart_10.dart
@@ -1,1 +1,17 @@
+// 3 operator null safety  ? , !  , late . hanya di dart >= 2.12
+
+void main() {
+  
+  // String nama;   //akan error karena variable nama tidak bisa dihitung jumlah karakternya
+
+  String nama2 = "Halo";   // variable nama bisa dihitung jumlah karakternya
+
+  String? nama; //tidak error dengan menggunakan String?
+  
+  late String nama3; //value nama3 akan diisi belakangan
+  
+  nama3 = 'hadi';
+  print(nama3);
+  
+}
 
