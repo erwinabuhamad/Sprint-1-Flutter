1ce89b8a2d8c773f54dd03c622db5d7738b143fc
erwinabuhamad <128515930+erwinabuhamad@users.noreply.github.com>
2023-03-25 14:32:40 +0400

    Looping FOR, WHILE, DO WHILE

--- dart_11.dart
+++ dart_11.dart
@@ -1,1 +1,40 @@
+//Perulangan LOOP
+
+void main() {
+ 
+  
+  //PENGGUNAAN FOR
+  print('-----For------');
+  
+  for(int i=0; i<=10; i++)
+  {
+    print(i);
+  }
+  
+  
+  //PENGGUNAAN WHILE
+  print('-----While------');
+  
+  int nilaiAwal = 0;
+  int nilaiAkhir = 10;
+  
+  while(nilaiAwal <= nilaiAkhir)
+  {
+    print(nilaiAwal);
+    nilaiAwal++;
+  }
+  
+  
+  //PENGGUNAAN DO WHILE
+  print('-----Do While------');
+  int hitung = 1;
+  do
+  {
+    print (hitung);
+    hitung++;
+    
+  }
+  while (hitung <=10);
+  
+}
 
